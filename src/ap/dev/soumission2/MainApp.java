e0fa3cb6f2672829c66cbd3506a35ac0e73d535d
Adrien <adrien@sion>
2018-05-19 23:49:34 +0200

    - LOG

--- src/ap/dev/soumission2/MainApp.java
+++ src/ap/dev/soumission2/MainApp.java
@@ -5,6 +5,8 @@ import ap.dev.soumission2.model.M_CAN;
 import ap.dev.soumission2.tools.Log;
 import ap.dev.soumission2.view.RootLayoutController;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,11 +15,16 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
     
+    private final static Logger LOGGER = Logger.getLogger(MainApp.class.getName());
+    
     private static M_CAN can = new M_CAN();
     
     @Override
     public void start(Stage primaryStage) {  
-
+    
+        /*Logger.getLogger("").setLevel(Level.INFO);
+        Logger.getLogger("").getHandlers()[0].setLevel(Level.INFO);*/
+        
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("/ap/dev/soumission2/view/RootLayout.fxml"));
@@ -29,10 +36,20 @@ public class MainApp extends Application {
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
-            Log.msg(1, "Error RootLayout.fxml | " + e.getMessage());
+            LOGGER.severe("Error RootLayout.fxml | " + e.getMessage());
         }                
 
         Can_Xml.load();
+        
+       /*LOGGER.severe("severe");
+       LOGGER.info("info");
+       LOGGER.warning("warning");
+       LOGGER.fine("fine");
+       LOGGER.setLevel(Level.ALL);
+       LOGGER.warning(LOGGER.getLevel().getName());
+       LOGGER.fine("fine2");*/
+
+       
         //Log.msg(0, can.toString());
         //Can_Xml.save();
         
